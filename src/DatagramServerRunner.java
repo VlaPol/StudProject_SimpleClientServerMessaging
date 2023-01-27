f4dac59cc48d456030234b9ddb04bee48a469ce0
uladzislau.polubok <uladzislau.polubok@belstat.local>
2023-01-27 17:53:23 +0300

    feat: add datagram messaging

---
+++ src/DatagramServerRunner.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+
+public class DatagramServerRunner {
+
+    public static void main(String[] args) throws IOException {
+
+        try(var datagramServer= new DatagramSocket(7777)){
+
+            var buffer = new byte[21];  // size should be equal ore more than send packet
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+            datagramServer.receive(packet);
+
+            System.out.println(new String(buffer));
+        }
+    }
+}
