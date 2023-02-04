790b6ae526c35746c6cff06e492ff3642bc21e30
pva <tango2002@gmail.com>
2023-02-04 20:38:58 +0300

    fix: fix some mistakes

--- src/http/server/HttpClientRunner.java
+++ src/http/server/HttpClientRunner.java
@@ -14,24 +14,19 @@ public class HttpClientRunner {
 
     public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
 
-        // create http-client
         var httpClient = HttpClient.newBuilder()
                 .version(HttpClient.Version.HTTP_1_1)
                 .build();
 
-        // create new http-request
         var httpRequest = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:9000"))                      // where we should send request
-                .header("content-type", "application/json")
+                .uri(URI.create("http://localhost:9000"))
+                .header("Content-Type", "application/json")
                 .POST(ofFile(Path.of("resources", "first.json")))
                 .build();
 
-        // create variable for received response
         var response = httpClient.sendAsync(httpRequest, HttpResponse.BodyHandlers.ofString());
         var response2 = httpClient.sendAsync(httpRequest, HttpResponse.BodyHandlers.ofString());
         var response3 = httpClient.sendAsync(httpRequest, HttpResponse.BodyHandlers.ofString());
-//        System.out.println(response.headers());
-//        System.out.println(response.body());
         System.out.println(response3.get().body());
     }
 }
