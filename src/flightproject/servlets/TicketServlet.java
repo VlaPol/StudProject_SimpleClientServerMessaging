f049eced03c58e86132c9c0036e007ed1dade559
uladzislau.polubok <uladzislau.polubok@belstat.local>
2023-02-13 11:22:16 +0300

    feat: add TicketServlet
    added servlet for getting tickets by flight number

---
+++ src/flightproject/servlets/TicketServlet.java
@@ -0,0 +1,40 @@
+package flightproject.servlets;
+
+import flightproject.dto.TicketDto;
+import flightproject.service.TicketService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet("/tickets")
+public class TicketServlet extends HttpServlet {
+
+    protected final TicketService ticketService = TicketService.getInstance();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        var flightId = Long.valueOf(req.getParameter("flightId"));
+
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
+
+        try(var printWriter = resp.getWriter()){
+            printWriter.write("<h1>Купленные билеты</h1>");
+            printWriter.write("<ul>");
+            for(TicketDto ticketDto: ticketService.findAllByFlightId(flightId)){
+                printWriter.write("""
+                   <li>
+                        %s
+                   </li>
+                """.formatted(ticketDto.getSeatNo()));
+            }
+            printWriter.write("</ul>");
+        }
+    }
+}
