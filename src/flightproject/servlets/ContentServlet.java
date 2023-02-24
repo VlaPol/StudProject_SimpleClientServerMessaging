d194dd41d2dc21a7a99c7f3818a9b8c658dc2788
uladzislau.polubok <uladzislau.polubok@belstat.local>
2023-02-24 09:08:15 +0300

    feat: modify content servlet with jsp pages

--- src/flightproject/servlets/ContentServlet.java
+++ src/flightproject/servlets/ContentServlet.java
@@ -1,5 +1,7 @@
 package flightproject.servlets;
 
+import flightproject.dto.FlightDto;
+import flightproject.service.FlightService;
 import flightproject.util.JspHelper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -8,13 +10,22 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
+
+import static java.util.stream.Collectors.*;
 
 @WebServlet("/content")
 public class ContentServlet extends HttpServlet {
 
+    public static FlightService flightService = FlightService.getInstance();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<FlightDto> flightDtos = flightService.findAll();
+        req.setAttribute("flights", flightDtos);
+        req.getSession().setAttribute("flightsMap", flightDtos.stream().collect(toMap(FlightDto::getId, FlightDto::getDescription)));
+
         req.getRequestDispatcher(JspHelper.getPath("content"))
-                .forward(req,resp);
+                .forward(req, resp);
     }
 }
--- web/WEB-INF/jsp/content.jsp
+++ web/WEB-INF/jsp/content.jsp
@@ -5,7 +5,19 @@
 </head>
 <body>
     <%@include file="header.jsp"%>
-    <div>Content. Русский</div>
+
+    <div>
+        <span>Content. Русский</span>
+        <p>Size: ${requestScope.flights.size()}</p>
+        <p>Id: ${requestScope.flights[1].id}</p>
+        <p>Map Id: ${requestScope.flightsMap[1]}</p>
+        <p>JSESSIONID: ${cookie["JSESSIONID"].value}, unique identifier</p>
+        <p>Header: ${header["Cookie"]}</p>
+        <p>Param id: ${param.id}</p>
+        <p>Param test: ${param.test}</p>
+        <p>Empty list: ${not empty requestScope.flights}</p>
+    </div>
+
     <%@include file="footer.jsp"%>
 </body>
 </html>
